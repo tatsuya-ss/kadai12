9219677152fea0e6075c468416eb10b25f323b10
tatsuya-ss <72326299+tatsuya-ss@users.noreply.github.com>
2020-12-30 11:21:09 +0900

    NSUserDefaultを使用してデータの保存機能の追加

--- kadai12/ViewController.swift
+++ kadai12/ViewController.swift
@@ -7,22 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
     @IBOutlet private var zeinukiTextField: UITextField!
     @IBOutlet private var zeirituTextField: UITextField!
     @IBOutlet private var kekkaLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let zei = UserDefaults.standard.string(forKey: "zei") {
+            self.zeirituTextField.text = zei
+        }
     }
     
-    @IBAction func culculateButton(_ sender: Any) {
+    @IBAction private func culculateButton(_ sender: Any) {
         let zeinuki = Double(zeinukiTextField.text!) ?? 0
-        let zeiritu = 1 + (Double(zeirituTextField.text!) ?? 0) / 100 
+        let zeiritu = 1 + (Double(zeirituTextField.text!) ?? 0) / 100
         let kekka = zeinuki * zeiritu
         kekkaLabel.text = "\(Int(kekka))円"
+        
+        UserDefaults.standard.setValue(self.zeirituTextField.text, forKey: "zei")
     }
 }
 
