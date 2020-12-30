49131a63c2e5e0862ef8dfd0b070f1774d33041a
tatsuya-ss <72326299+tatsuya-ss@users.noreply.github.com>
2020-12-30 10:43:06 +0900

    計算機能を追加

--- kadai12/ViewController.swift
+++ kadai12/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private var zeinukiTextField: UITextField!
+    @IBOutlet private var zeirituTextField: UITextField!
+    @IBOutlet private var kekkaLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func culculateButton(_ sender: Any) {
+        let zeinuki = Double(zeinukiTextField.text!) ?? 0
+        let zeiritu = 1 + (Double(zeirituTextField.text!) ?? 0) / 100 
+        let kekka = zeinuki * zeiritu
+        kekkaLabel.text = "\(Int(kekka))円"
+    }
 }
 
