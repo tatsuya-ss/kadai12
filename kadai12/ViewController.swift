172661426fd4608cdad3211f0cc0697504b6d5ec
tatsuya-ss <72326299+tatsuya-ss@users.noreply.github.com>
2020-12-30 10:01:33 +0900

    レイアウト作成

---
+++ kadai12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kadai12
+//
+//  Created by 坂本龍哉 on 2020/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
